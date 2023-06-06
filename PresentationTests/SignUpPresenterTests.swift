11f6444f7b2ad75624ba4ea98833a45eada9a47b
Diego Ferreira <diferodrigues@gmail.com>
2023-06-05 21:16:48 -0300

    Move validation to a method

--- PresentationTests/SignUpPresenterTests.swift
+++ PresentationTests/SignUpPresenterTests.swift
@@ -9,21 +9,29 @@ class SignUpPresenter {
     }
 
     func signUp(viewModel: SignUpViewModel) {
+        if let errorMessage = validate(viewModel: viewModel) {
+            alertView.showMessage(viewModel: AlertViewModel(title: "Falha na validação", message: errorMessage))
+        }
+    }
+
+    private func validate(viewModel: SignUpViewModel) -> String? {
         if viewModel.name == nil || viewModel.name!.isEmpty {
-            alertView.showMessage(viewModel: AlertViewModel(title: "Falha na validação", message: "O campo nome é obrigatório"))
+            return "O campo nome é obrigatório"
         }
 
         else if viewModel.email == nil || viewModel.email!.isEmpty {
-            alertView.showMessage(viewModel: AlertViewModel(title: "Falha na validação", message: "O campo email é obrigatório"))
+            return "O campo email é obrigatório"
         }
 
         else if viewModel.password == nil || viewModel.password!.isEmpty {
-            alertView.showMessage(viewModel: AlertViewModel(title: "Falha na validação", message: "O campo senha é obrigatório"))
+            return "O campo senha é obrigatório"
         }
 
         else if viewModel.passwordConfirmation == nil || viewModel.passwordConfirmation!.isEmpty {
-            alertView.showMessage(viewModel: AlertViewModel(title: "Falha na validação", message: "O campo confirmar senha é obrigatório"))
+            return "O campo confirmar senha é obrigatório"
         }
+
+        return nil
     }
 }
 
