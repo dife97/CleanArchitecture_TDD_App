b1f1abe950bfe37c1944d150a5cf4491cf49e854
Diego Ferreira <diferodrigues@gmail.com>
2023-06-06 21:55:28 -0300

    Rename SignUp storyboard

--- UITests/SignUpViewControllerTests.swift
+++ UITests/SignUpViewControllerTests.swift
@@ -5,8 +5,8 @@ import UIKit
 final class SignUpViewControllerTests: XCTestCase {
 
     func test_loading_is_hidden_on_start() {
-        let storyboard = UIStoryboard(name: "SignUpStoryboard", bundle: Bundle(for: SignUpViewController.self))
-        let sut = storyboard.instantiateViewController(withIdentifier: "SignUpViewControllerID") as! SignUpViewController
+        let storyboard = UIStoryboard(name: "SignUp", bundle: Bundle(for: SignUpViewController.self))
+        let sut = storyboard.instantiateViewController(withIdentifier: "SignUpViewController") as! SignUpViewController
 
         sut.loadViewIfNeeded()
 
