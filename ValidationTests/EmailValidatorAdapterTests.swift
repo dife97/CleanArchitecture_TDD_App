ed8e9f5c07b7e3dcb10d6fe7ecfb379c3ebd3f7b
Diego Ferreira <diferodrigues@gmail.com>
2023-06-13 23:19:29 -0300

    Create makeSUT factory method

--- ValidationTests/EmailValidatorAdapterTests.swift
+++ ValidationTests/EmailValidatorAdapterTests.swift
@@ -4,7 +4,7 @@ import Presentation
 final class EmailValidatorAdapterTests: XCTestCase {
 
     func test_invalid_emails() {
-        let sut = EmailValidatorAdapter()
+        let sut = makeSUT()
         XCTAssertFalse(sut.isValid(email: "aa"))
         XCTAssertFalse(sut.isValid(email: "a@"))
         XCTAssertFalse(sut.isValid(email: "a@a"))
@@ -13,11 +13,16 @@ final class EmailValidatorAdapterTests: XCTestCase {
     }
 
     func test_valid_emails() {
-        let sut = EmailValidatorAdapter()
+        let sut = makeSUT()
         XCTAssertTrue(sut.isValid(email: "diego@gmail.com"))
         XCTAssertTrue(sut.isValid(email: "diego@hotmail.com"))
         XCTAssertTrue(sut.isValid(email: "diego@hotmail.com.br"))
     }
+}
+
+extension EmailValidatorAdapterTests {
+
+    private func makeSUT() -> EmailValidatorAdapter { EmailValidatorAdapter() }
 }
 
 public final class EmailValidatorAdapter: EmailValidator {
