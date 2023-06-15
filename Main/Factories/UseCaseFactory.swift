9b7fa7d448e72e11baa5d64c87d21d462bb18a11
Diego Ferreira <diego.rodrigues4@raizen.com>
2023-06-14 22:28:08 -0300

    Add Environment Variables

--- Main/Factories/UseCaseFactory.swift
+++ Main/Factories/UseCaseFactory.swift
@@ -6,7 +6,7 @@ import Infra
 final class UseCaseFactory {
 
     private static let httpClient = URLSessionAdapter()
-    private static let apiBaseURL = "https://clean-node-api.herokuapp.com/api"
+    private static let apiBaseURL = Environment.variable(.apiBaseURL)
 
     private static func makeURL(path: String) -> URL {
         URL(string: "\(apiBaseURL)\(path)")!
--- MainTests/SignUpIntegrationTests.swift
+++ MainTests/SignUpIntegrationTests.swift
@@ -4,6 +4,10 @@ import Main
 final class SignUpIntegrationTests: XCTestCase {
 
     func test_UI_presentation_integration() {
+        debugPrint("============")
+        debugPrint(Environment.variable(.apiBaseURL))
+        debugPrint("============")
+
         let sut = SignUpComposer.composeController(with: AddAccountSpy())
         checkMemoryLeak(for: sut)
     }
