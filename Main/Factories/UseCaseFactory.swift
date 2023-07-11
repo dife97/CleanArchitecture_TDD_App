c4d2109586cda6446aa99864d4d5f87340179700
Diego Ferreira <diferodrigues@gmail.com>
2023-07-10 21:36:58 -0300

    Move MainQueueDispatchDecorator to its own file and group

--- Main/Factories/UseCaseFactory.swift
+++ Main/Factories/UseCaseFactory.swift
@@ -21,29 +21,3 @@ final class UseCaseFactory {
         return MainQueueDispatchDecorator(remoteAddAccount)
     }
 }
-
-public final class MainQueueDispatchDecorator<T> {
-
-    private let instance: T
-
-    public init(_ instance: T) {
-        self.instance = instance
-    }
-
-    func dispatch(completion: @escaping () -> Void) {
-        guard Thread.isMainThread else { return DispatchQueue.main.async(execute: completion)}
-        completion()
-    }
-}
-
-extension MainQueueDispatchDecorator: AddAccount where T: AddAccount {
-
-    public func add(
-        account: AddAccountModel.Request,
-        onComplete: @escaping (Result<AddAccountModel.Response, DomainError>) -> Void
-    ) {
-        instance.add(account: account) { [weak self] result in
-            self?.dispatch { onComplete(result) }
-        }
-    }
-}
